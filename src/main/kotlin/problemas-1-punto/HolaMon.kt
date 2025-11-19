3e14018ee196b85ce5365cc096e9d78f910d4744
trinidad.diez.7e9 <diez.trinidad@hotmail.com>
2025-11-19 09:05:23 +0100

    Problema de un punto resuelto.

---
+++ src/main/kotlin/problemas-1-punto/HolaMon.kt
@@ -0,0 +1,13 @@
+/*
+    Entrada: -
+    Sortida: Hello World
+ */
+
+// Crea la funció principal.
+fun main() {
+    // Emmagatzema el missatge en una variable.
+    val missatge = "Hello World"
+
+    // Imprimeix el missatge com una línia a la pantalla.
+    println(missatge)
+}
