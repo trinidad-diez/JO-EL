405238c91f109b518363fb7de66b85efee9956d4
trinidad.diez.7e9 <diez.trinidad@hotmail.com>
2025-11-19 09:08:54 +0100

    Problema de un punto resuelto.

---
+++ src/main/kotlin/problemas-1-punto/Suma-Dos.kt
@@ -0,0 +1,18 @@
+import java.util.Scanner
+
+/*
+    Entrada: 2 nombres (Integers).
+    Sortida: nombre 1 + nombre 2.
+*/
+
+fun main(){
+    // Crea un escàner per llegir l'entrada de l'usuari.
+    val sc : Scanner = Scanner(System.`in`)
+
+    // Enmagatzema les entrada (els nombres) en una variable.
+    val nombre1 : Int = sc.nextInt()
+    val nombre2 : Int = sc.nextInt()
+
+    // Imprimeix el nombre a la consola.
+    println(nombre1 +  nombre2)
+}
