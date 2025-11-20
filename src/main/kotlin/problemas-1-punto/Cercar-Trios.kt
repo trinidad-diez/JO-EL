3b6a2bbfe999f3aacf841ab13c2221f9c73aad77
trinidad-diez <diez.trinidad@hotmail.com>
2025-11-20 10:37:07 +0100

    Problema de un punt ressolt.

--- src/main/kotlin/problemas-1-punto/Cercar-Trios.kt
+++ src/main/kotlin/problemas-1-punto/Cercar-Trios.kt
@@ -4,27 +4,29 @@ import java.util.Scanner
 
 /*
     Entrada:
-        1) C1 (1-13, Integer)
-        2) C2 (1-13, Integer)
-        3) C3 (1-13, Integer)
+        1) A,B,C (> 18, Integer)
+        2) D (> 18, Integer)
 
-    Sortida: "SI" si les tres cartes son iguals i "NO" en cas contrari
+    Sortida: SI si D no té l'edat de A,B i C, i NO en cas contrari.
  */
 fun main(){
     // Crea un escàner per llegir l'entrada de l'usuari.
     val sc : Scanner = Scanner(System.`in`)
 
     // Enmagatzema els tres nombres en una variable.
-    val c1 : Int = sc.nextInt()
-    val c2 : Int = sc.nextInt()
-    val c3 : Int = sc.nextInt()
+    val edata : Int = sc.nextInt()
+    val edatb : Int = sc.nextInt()
+    val edatc : Int = sc.nextInt()
+    val edatd : Int = sc.nextInt()
 
-    // Es fixa si és any de trespàs (divisible per 4).
-    if ((c1 == c2) && (c2 == c3)) {
-        println("SI")
-    } else {
-        println("NO")
+    var resposta = "SI"
+
+    // Comprara l'edat D amb les anteriors.
+    if(edata == edatd || edatb == edatd || edatc == edatd){
+        resposta = "NO"
     }
+
+    println(resposta)
 
     // Tanca l'escàner.
     sc.close()
