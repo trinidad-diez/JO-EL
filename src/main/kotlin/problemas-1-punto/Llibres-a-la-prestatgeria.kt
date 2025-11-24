31074b25d00df96cbe524048a67238f7226a253d
trinidad-diez <diez.trinidad@hotmail.com>
2025-11-24 13:30:28 +0100

    Problema d'un punt ressolt.

---
+++ src/main/kotlin/problemas-1-punto/Llibres-a-la-prestatgeria.kt
@@ -0,0 +1,31 @@
+package `problemas-1-punto`
+
+import java.util.Scanner
+
+/*
+    Entrada: 3 nombres enters: N (prestatges), M (capacitat de cada prestatge), K (total llibres que volem posar)
+    Sortida: total de llibres que no han cabut als prestatges.
+*/
+
+fun main(){
+    // Crea un escàner per llegir l'entrada de l'usuari.
+    val sc : Scanner = Scanner(System.`in`)
+
+    // Emmagatzema les entrades en variables.
+    var n : Int = sc.nextInt()
+    var m : Int = sc.nextInt()
+    var k : Int = sc.nextInt()
+
+    // Calcula l'espai total per llibres.
+    val espaiTotal : Int = n * m
+
+    // Imprimeix total de llibres que no han cabut als prestatges a la consola.
+    if ((espaiTotal == 0)|| (k == 0)){
+        println(k)
+    } else if (espaiTotal > k){
+        println(0)
+    } else {
+        println((espaiTotal - k)* -1)
+    }
+
+// Tanca l'escàner.
