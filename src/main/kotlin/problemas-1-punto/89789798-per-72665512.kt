2c048559b7c115f5b7f2a9b93dcc5d86c9b05c47
trinidad.diez.7e9 <diez.trinidad@hotmail.com>
2025-11-24 12:01:18 +0100

    Resolució problema d'un punt.

---
+++ src/main/kotlin/problemas-1-punto/89789798-per-72665512.kt
@@ -0,0 +1,13 @@
+package `problemas-1-punto`
+
+fun main(){
+    //Emmagatzema els dos nombres en variables.
+    val num1 : Int = 89789798;
+    val num2 : Int = 72665512;
+
+    // Emmagatzema la multiplicació dels dos nombres en una variable.
+    val multiplicacio : Int = num1 * num2;
+
+    // Imprimeix la multiplicació a la consola.
+    println("El resultat es: ${multiplicacio}")
+}
