55dd040ded37b4253468c190ae0b5d5b0d66893b
trinidad.diez.7e9 <diez.trinidad@hotmail.com>
2025-11-25 10:10:12 +0100

    Resolució problema d'un punt.

--- src/main/kotlin/problemas-1-punto/Una-de-cada.kt
+++ src/main/kotlin/problemas-1-punto/Una-de-cada.kt
@@ -1,2 +1,38 @@
 package `problemas-1-punto`
+import java.util.Scanner
 
+/*
+
+    Entrada:
+        1) N
+        2) Lineas
+
+    Sortida: n línies de text contenint el mateix text que s'ha llegit, però primer minúsucula i després majúscula.
+*/
+
+fun main(){
+    // Crea un escàner per llegir l'entrada de l'usuari.
+    val sc : Scanner = Scanner(System.`in`)
+
+    // Enmagatzema la entrada en una variable.
+    val nombreDeLineas : Int = sc.nextInt()
+    sc.nextLine()
+
+    // repeteix el bucle tantes vegades com indiqui l'entrada de l'usuari.
+    repeat(nombreDeLineas){
+        val linea = sc.nextLine()
+        var nuevaLinea = ""
+
+        for (i in linea.indices){
+            if (i % 2 == 0){
+                nuevaLinea += linea[i].lowercase()
+            } else {
+                nuevaLinea += linea[i].uppercase()
+            }
+        }
+        println(nuevaLinea)
+    }
+
+    // Tanca l'escàner.
+    sc.close()
+}
