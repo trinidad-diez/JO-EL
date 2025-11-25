574402b647b13ad1c6745f31f8f74052e4d2422e
trinidad.diez.7e9 <diez.trinidad@hotmail.com>
2025-11-25 10:22:02 +0100

    Resolució problema d'un punt.

--- src/main/kotlin/problemas-1-punto/Una-de-cada.kt
+++ src/main/kotlin/problemas-1-punto/Una-de-cada.kt
@@ -1,38 +1,64 @@
-package `problemas-1-punto`
 import java.util.Scanner
 
 /*
-
     Entrada:
-        1) N
-        2) Lineas
-
-    Sortida: n línies de text contenint el mateix text que s'ha llegit, però primer minúsucula i després majúscula.
+        1) Número de casos (0-1000)
+        Per cada cas:
+            * P/K/S/C
+            * text complet
+    Sortida:
+        text convertit segons la notació indicada
 */
 
-fun main(){
+fun main() {
     // Crea un escàner per llegir l'entrada de l'usuari.
-    val sc : Scanner = Scanner(System.`in`)
+    val sc = Scanner(System.`in`)
 
     // Enmagatzema la entrada en una variable.
-    val nombreDeLineas : Int = sc.nextInt()
+    val nombreDeCasos = sc.nextInt()
     sc.nextLine()
 
-    // repeteix el bucle tantes vegades com indiqui l'entrada de l'usuari.
-    repeat(nombreDeLineas){
-        val linea = sc.nextLine()
-        var nuevaLinea = ""
+    // Repeteix el bucle tantes vegades com nombre de casos.
+    repeat(nombreDeCasos) {
+        val tipus = sc.next()
+        sc.nextLine()
+        val linea = sc.nextLine().trimStart()
 
-        for (i in linea.indices){
-            if (i % 2 == 0){
-                nuevaLinea += linea[i].lowercase()
-            } else {
-                nuevaLinea += linea[i].uppercase()
+        when (tipus) {
+            "P" -> { // PascalCase
+                val paraules = linea.split(" ").filter { it.isNotEmpty() }
+                val resultat = paraules.joinToString("") { p ->
+                    val min = p.lowercase()
+                    min.replaceFirstChar { c -> c.uppercaseChar() }
+                }
+                println(resultat)
             }
+
+            "K" -> println(linea.lowercase().replace(" ", "-"))
+
+            "S" -> println(linea.lowercase().replace(" ", "_"))
+
+            "C" -> { // CamelCase
+                val paraules = linea.split(" ").filter { it.isNotEmpty() }
+                val sb = StringBuilder()
+
+                for ((i, p) in paraules.withIndex()) {
+                    val min = p.lowercase()
+                    if (i == 0) {
+                        sb.append(min) // primera paraula en minúscules
+                    } else {
+                        sb.append(
+                            min.replaceFirstChar { c -> c.uppercaseChar() }
+                        )
+                    }
+                }
+
+                println(sb.toString())
+            }
+
+            else -> println(linea)
         }
-        println(nuevaLinea)
     }
 
-    // Tanca l'escàner.
     sc.close()
 }
