48ad4e66329e434e99874ee6b0b9fa1bee6f86a1
trinidad-diez <diez.trindad@hotmail.com>
2025-11-28 08:24:45 +0100

    Resolució problema d'un punt.

---
+++ src/main/kotlin/problemas-1-punto/El-meu-primer-bucle.kt
@@ -0,0 +1,23 @@
+package `problemas-1-punto`
+import java.util.Scanner
+
+/*
+    Entrada:
+        1) Casos de prova a considerar.
+        2) Nombre (Integer).
+ */
+
+fun main(){
+    // Crea un escàner per llegir l'entrada de l'usuari.
+    val sc : Scanner = Scanner(System.`in`)
+
+    // Emmagatzema el nombre de cassos de prova en una variable.
+    val nombreCassos : Int = sc.nextInt()
+
+    // Repteix el bucle tantes vegades com casos de prova.
+    repeat (nombreCassos){
+        var nombre : Int = sc.nextInt()
+        nombre ++
+        println(nombre)
+    }
+}
